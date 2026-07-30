fafb98ef38009e552848c5afa9869055ba9c2592
Dima7TW <admin@techweb.kh.ua>
2026-07-30 20:20:43 +0300

    ubuntu 24.04 build image fix

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 SRC="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 OUTPUT="${SRC}/out"
 mkdir -p "${OUTPUT}"
-docker buildx build --output "type=local,dest=${OUTPUT}" --progress=plain -t kodi:gbm .
+docker buildx build --platform linux/arm64 --output "type=local,dest=${OUTPUT}" --progress=plain -t kodi:gbm .
 echo "Done!"
 ls -laht "${OUTPUT}"
 
