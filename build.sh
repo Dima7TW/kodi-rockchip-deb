29314678db61fad96e05cd829b24c1061dd5ad25
Dima7TW <admin@techweb.kh.ua>
2026-08-05 19:37:17 +0300

    build fix

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 SRC="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 OUTPUT="${SRC}/out"
 mkdir -p "${OUTPUT}"
-docker buildx build --target linux/arm64 --output "type=local,dest=${OUTPUT}" --progress=plain -t kodi:gbm .
+docker buildx build --platform linux/arm64 --output "type=local,dest=${OUTPUT}" --progress=plain -t kodi:gbm .
 echo "Done!"
 ls -laht "${OUTPUT}"
